2389b73878a6c44aea42d6b9468bdfc944683e13
Jeronimo Silva <shaolin.jr@gmail.com>
2026-05-25 10:39:56 -0300

    Arquivos antigos adicionados ao .gitignore

--- arquivo.py
+++ arquivo.py
@@ -2,26 +2,26 @@
 import csv
 
 # Arquivo onde constam os contatos, com cabeçalho, em formato csv
+# Aprendi que arquivos de caminho costumam ter variáveis ALL CAPS relacionadas
 ARQUIVO_AGENDA = 'agenda.csv'
 
 # Separa o arquivo de agenda em cabeçalho e contatos.
 # Remove contatos vazios ou sem nome
 def desmembrar():
-    try:
-        with open(ARQUIVO_AGENDA, 'r', newline='', encoding='utf-8') as agenda:
-            reader = csv.reader(agenda, delimiter=',')
-            tudao = list(reader)
-            if not tudao:
-                return ['Nome', 'Email', 'Telefone'], []
-            cabecalho = tudao[0]
-            contatos = tudao[1:]
-            # Remove as linhas vazias:
-            # se a linha estiver vazia, o 'if linha' retorna falso,
-            # e ela não entra nos contatos limpos
-            contatos_limpos = [linha for linha in contatos if len(linha) > 0 and linha[0] != ""]
-            return cabecalho, contatos_limpos
-    except FileNotFoundError:
-        return ['Nome', 'Email', 'Telefone'], []
+    with open(ARQUIVO_AGENDA, 'r', newline='', encoding='utf-8') as agenda:
+        reader = csv.reader(agenda, delimiter=',')
+        tudao = list(reader)
+        cabecalho = tudao[0]
+        contatos = tudao[1:]
+        # Remove as linhas vazias:
+        # se a linha estiver vazia, o 'if linha' retorna falso,
+        # e ela não entra nos contatos limpos
+        contatos_limpos = [linha for linha in contatos if linha[0] != ""]
+        # contatos_limpos = [linha for linha in contatos if len(linha) > 0 and linha[0] != ""]
+
+        return cabecalho, contatos_limpos
+
+
 
 # Une cabeçalho e contatos para gerar um arquivo de agenda.
 def reconstruir_agenda(campos, contatos):
